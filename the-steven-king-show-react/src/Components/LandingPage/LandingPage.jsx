2e374d02ed1cd3fd5a6c0b491ff18d9d1aadfffd
Steven King <kingsteven1113@gmail.com>
2024-02-05 22:05:16 -0500

    Creating Layout

---
+++ the-steven-king-show-react/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,25 @@
+import "./LandingPage.css"
+import "../../assets/HelloBubbles.svg"
+
+const LandingPage = () => {
+
+    return (
+    <>
+     <div className='HeroSection'>
+        <div className='HeroSectionImage'>
+          <img src="Hello Bubbles.svg" alt="" />
+        </div>
+        <div className='HeroSectionInformation'>
+        <h1>I'm Steven King</h1>
+        <h2>Front End Developer <br></br>& UX/UI Designer</h2>
+        <p></p>
+        </div>
+      </div>
+    <div className='BodyContent'>
+
+        </div>
+        </> 
+    )
+}
+
+export default LandingPage;
---
+++ the-steven-king-show-react/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import "./NavBar.css"
+import Logo from '../../assets/Logo.svg'
+
+
+
+
+const NavBar = () => {
+
+    const [count, setCount] = useState(0)
+
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const onScroll = () => {
+    // This will calculate how many pixels the page is vertically
+    const winScroll = document.documentElement.scrollTop;
+    // This is responsible for subtracticing the total height of the page - where the users page is scrolled to
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    // This will calculate the final total of the percentage of how much the user has scrolled.
+    const scrolled = (winScroll / height) * 100;
+
+    setScrollTop(scrolled);
+  };
+
+  useEffect(() => {
+    // Fires when the document view has been scrolled
+    window.addEventListener("scroll", onScroll);
+
+    // 
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+    return (
+        <>
+        <div className='NavBar' id='NavBar'>
+        <div className='NavBarLogo'>
+          <img src={Logo}alt="" />
+        </div>
+        <nav>
+            <li>
+             About Me
+            </li>
+            <li>
+            Projects
+            </li>
+            <li>
+            Aspirations
+            </li>
+          </nav>
+          <div className='ContactButton'>Contact Me</div>
+      </div>
+  
+      <div className="progressMainWrapper">
+          <div
+            className="progressMainStyle"
+            style={{ height: `${scrollTop}%` }}
+          ></div>
+        
+        </div>
+
+        </>
+       
+    )
+}
+
+export default NavBar;
