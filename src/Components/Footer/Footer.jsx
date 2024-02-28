39b9f6f63705315b6c37e1f78871431d078a7ae4
Steven King <kingsteven1113@gmail.com>
2024-02-27 23:42:45 -0500

    Responsive Page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     return (
        <>
        <div className='Footer'>
-        <img src={Logo} />
+        <img className='Logo' src={Logo} />
         <ul className='Contacts'>
             <li><a href="https://www.linkedin.com/in/stevenkingdesign/">LinkedIn</a></li>
             <li><a href="https://github.com/kingsteven1113">GitHub</a></li>
